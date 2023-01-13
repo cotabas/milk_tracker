fbe859f988f7ff5dd5912672dcaceb490d1ea364
cotabas <cotabas@gmail.com>
2023-01-12 23:52:35 -0800

    added a stats page

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,32 +1,16 @@
 class MealsController < ApplicationController
   http_basic_authenticate_with name: Rails.application.credentials.dig(:basic_auth, :user), password: Rails.application.credentials.dig(:basic_auth, :pass)
   before_action :set_meal, only: %i[ show edit update destroy ]
+  before_action :hash_meal, only: %i[ index stats ]
 
   # GET /meals or /meals.json
   def index
-    @meals = Meal.all.sort
     @meal = Meal.new
     @day = []
-    @meal_hash = Hash.new
-    @meals.each do |meal|
-      @meal_hash[meal.created_at.strftime("%m/%d")] = [] unless @meal_hash.include?(meal.created_at.strftime("%m/%d"))
-      @meal_hash[meal.created_at.strftime("%m/%d")] << [meal.food, meal.quantity]
-    end
-    @meal_hash.each do |date, val|
-      hash = {
-        'formula' => 0, 
-        'bank' => 0,
-        'breast' => 0,
-        :total => 0 }
-      val.each do |feed| 
-        hash[feed[0]] += feed[1]
-        hash[:total] += feed[1]
-      end
-
-      @meal_hash[date] = hash
-    end
   end
 
+  def stats
+  end
   # GET /meals/1 or /meals/1.json
   def show
   end
@@ -87,6 +71,28 @@ class MealsController < ApplicationController
       @meal = Meal.find(params[:id])
     end
 
+    def hash_meal
+      @meals = Meal.all.sort
+      @meal_hash = Hash.new
+      @meals.each do |meal|
+        @meal_hash[meal.created_at.strftime("%m/%d")] = [] unless @meal_hash.include?(meal.created_at.strftime("%m/%d"))
+        @meal_hash[meal.created_at.strftime("%m/%d")] << [meal.food, meal.quantity]
+      end
+      @meal_hash.each do |date, val|
+        hash = {
+          'formula' => 0, 
+          'bank' => 0,
+          'breast' => 0,
+          :total => 0 }
+        val.each do |feed| 
+          hash[feed[0]] += feed[1]
+          hash[:total] += feed[1]
+        end
+  
+        @meal_hash[date] = hash
+      end
+    end
+    
     # Only allow a list of trusted parameters through.
     def meal_params
       params.require(:meal).permit(:quantity, :food, :fed_time)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "meals#index"
+  
+  get '/stats', to: 'meals#stats'
 
   # Defines the root path route ("/")
   # root "articles#index"
